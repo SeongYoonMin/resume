34d7c08e9c2f9b8df0115d11cd632b1efc4084e1
SeongYoonMin <namo4862@naver.com>
2023-07-20 19:02:57 +0900

    Fix: Add config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  typescript: {
+    shim: false,
+  },
+  devtools: { enabled: true },
+  app: {
+    head: {
+      htmlAttrs: {
+        lang: "kr",
+      },
+      title: "함께하고싶은 사람을 꿈꾸다.",
+    },
+  },
+  css: ["~/assets/scss/main.scss", "~/assets/scss/reset.scss"],
+});
